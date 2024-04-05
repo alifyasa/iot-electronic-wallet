a5c53665e9ef37923c033168fc1aea2b8a3e2a4b
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-04-05 14:13:22 +0700

    fix: Rename wallet variable

--- cmd/wallet.go
+++ cmd/wallet.go
@@ -6,23 +6,23 @@ import (
 	"time"
 )
 
-var walletAmount = 235
+var WalletBalance = 235
 
 func CheckWalletHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%v", walletAmount)
+	fmt.Fprintf(w, "%v", WalletBalance)
 }
 
 func PayHandler(w http.ResponseWriter, r *http.Request) {
-	if walletAmount >= 20 {
-		walletAmount = walletAmount - 20
+	if WalletBalance >= 20 {
+		WalletBalance = WalletBalance - 20
 		msg := fmt.Sprintf(
 			"[%s] TRANSAKSI BERHASIL, SISA SALDO Rp.%v\n",
 			time.Now().Format("2006-01-02 15:04:05"),
-			walletAmount,
+			WalletBalance,
 		)
 		SendMessageToWebSockets(msg)
 		w.WriteHeader(200)
-		w.Write([]byte(fmt.Sprintf("%v", walletAmount)))
+		w.Write([]byte(fmt.Sprintf("%v", WalletBalance)))
 	} else {
 		msg := fmt.Sprintf(
 			"[%s] SALDO TIDAK MENCUKUPI.\n",
@@ -30,6 +30,6 @@ func PayHandler(w http.ResponseWriter, r *http.Request) {
 		)
 		SendMessageToWebSockets(msg)
 		w.WriteHeader(403)
-		w.Write([]byte(fmt.Sprintf("%v", walletAmount)))
+		w.Write([]byte(fmt.Sprintf("%v", WalletBalance)))
 	}
 }

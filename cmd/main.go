4b1369ab032daa20ac28a08c821600619220db9c
Alif Yasa <130131893+alifyasa@users.noreply.github.com>
2024-04-05 14:36:25 +0700

    fix: lock acquisition

--- cmd/main.go
+++ cmd/main.go
@@ -20,6 +20,10 @@ func main() {
 
 	loggedMux := LoggingMiddleware(mux)
 
+	fmt.Printf("IOT ELECTRONIC PAYMENT\n\n")
+	fmt.Printf("13520135 - Muhammad Alif Putra Yasa\n")
+	fmt.Printf("Dibuat untuk Ujian Tengah Semester IF4051 Semester 2 2023/2024\n\n")
+
 	log.Println("Server starting on port 8080")
 	log.Fatal(
 		http.ListenAndServe("0.0.0.0:8080", loggedMux),
--- cmd/wallet.go
+++ cmd/wallet.go
@@ -3,26 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 )
 
-var WalletBalance = 235
+var Wallet = struct {
+	sync.RWMutex
+	Balance float64
+}{Balance: 235}
 
 func CheckWalletHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "%v", WalletBalance)
+	Wallet.RLock()
+	fmt.Fprintf(w, "%v", Wallet.Balance)
+	Wallet.RUnlock()
 }
 
 func PayHandler(w http.ResponseWriter, r *http.Request) {
-	if WalletBalance >= 20 {
-		WalletBalance = WalletBalance - 20
+	Wallet.Lock()
+	if Wallet.Balance >= 20 {
+		Wallet.Balance -= 20
 		msg := fmt.Sprintf(
 			"[%s] TRANSAKSI BERHASIL, SISA SALDO Rp.%v\n",
 			time.Now().Format("2006-01-02 15:04:05"),
-			WalletBalance,
+			Wallet.Balance,
 		)
 		SendMessageToWebSockets(msg)
 		w.WriteHeader(200)
-		w.Write([]byte(fmt.Sprintf("%v", WalletBalance)))
+		w.Write([]byte(fmt.Sprintf("%v", Wallet.Balance)))
 	} else {
 		msg := fmt.Sprintf(
 			"[%s] SALDO TIDAK MENCUKUPI.\n",
@@ -30,6 +37,7 @@ func PayHandler(w http.ResponseWriter, r *http.Request) {
 		)
 		SendMessageToWebSockets(msg)
 		w.WriteHeader(403)
-		w.Write([]byte(fmt.Sprintf("%v", WalletBalance)))
+		w.Write([]byte(fmt.Sprintf("%v", Wallet.Balance)))
 	}
+	Wallet.Unlock()
 }
